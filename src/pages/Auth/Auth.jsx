6a4b79540fdbbe82b5596daf085e0554dcb00a64
suraj <surajkiranpanda98@gmail.com>
2024-12-02 23:09:09 +0530

    2nd commit

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -70,7 +70,7 @@ const Auth = () => {
   }
 
   return (
-    <div className="h-[100vh] w-[100vw] flex items-center justify-center">
+    <div className="h-[100vh] w-[100vw] flex items-center justify-center ">
       <div className="h-[80vh] bg-white border-white text-opacity-90 shadow-2xl w-[80vw] md:w-[90vw] lg:w-[70vw] xl:w-[60vw] rounded-3xl grid xl:grid-cols-2">
         <div className="flex flex-col gap-10 items-center justify-center">
           <div className="flex items-center justify-center flex-col">
@@ -78,7 +78,7 @@ const Auth = () => {
               <h1 className="text-5xl font-bold md:text-6xl">Welcome</h1>
               <img src={victory} alt="victory emoji" className="h-[100px]" />
             </div>
-            <p className="font-medium text-center">
+            <p className="font-semibold text-center text-blue-800">
               Fill in the details to get started with the best chat app
             </p>
           </div>
